39c4c84380c9f2a3c98f13111312b2de84d0f722
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2023-03-20 11:32:50 +0530

    CMS added

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,6 +16,7 @@ export default {
     disqusShortname: config.disqusShortname,
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
